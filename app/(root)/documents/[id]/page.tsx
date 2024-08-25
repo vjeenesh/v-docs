4dd2357234cc657a78c308c29df000eb82172671
Jinesh Van <vjeenesh@gmail.com>
2024-08-25 13:14:54 +0530

    minor update

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -3,6 +3,8 @@ import Header from "@/components/Header";
 import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import React from "react";
 
+
+
 const Document = () => {
   return (
     <div>
