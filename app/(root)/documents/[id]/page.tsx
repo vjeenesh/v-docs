b62fa396cf0b04090e4da9f65cf3be552124ac1c
Jinesh Van <vjeenesh@gmail.com>
2024-09-07 14:58:30 +0530

    added create new document with auth feature

--- app/(root)/documents/[id]/page.tsx
+++ app/(root)/documents/[id]/page.tsx
@@ -1,10 +1,12 @@
-import { Editor } from "@/components/editor/Editor";
-import Header from "@/components/Header";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+import CollaborativeRoom from "@/components/CollaborativeRoom";
 import React from "react";
 
 const Document = () => {
-  return <div></div>;
+  return (
+    <main className="flex w-full flex-col items-center">
+      <CollaborativeRoom />
+    </main>
+  );
 };
 
 export default Document;
